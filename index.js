c8f1a8bfe5c5e72ac91e1635e7c19c9167f6e4f4
mylle <kamyllevitoriamelo@gmail.com>
2025-11-19 06:41:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    const elementos = document.querySelectorAll('.scroll-animar');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('ativo');
+            }
+        });
+    }, { threshold: 0.2 });
+
+    elementos.forEach(el => observer.observe(el));
+
